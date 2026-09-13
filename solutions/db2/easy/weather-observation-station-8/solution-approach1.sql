2644774fd452f18bea7308a45f689431c640c259
DEVI G <gdevi250326@gmail.com>
2026-09-13 22:45:08 +0530

    ✅ Weather Observation Station 8 [db2] — approach1

---
+++ solutions/db2/easy/weather-observation-station-8/solution-approach1.sql
@@ -0,0 +1,22 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-8/problem?isFullScreen=true
+-- Problem     Weather Observation Station 8
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    db2
+-- Status      Accepted
+-- Submitted   2026-09-13, 10:45 p.m.
+-- ──────────────────────────────────────────────────
+
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LOWER(LEFT(CITY, 1)) IN ('a', 'e', 'i', 'o', 'u')
+  AND LOWER(RIGHT(CITY, 1)) IN ('a', 'e', 'i', 'o', 'u');
