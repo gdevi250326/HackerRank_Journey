9edba655300b3dc2b51ad1016c6a514e0a1cc13a
DEVI G <gdevi250326@gmail.com>
2026-09-20 21:38:50 +0530

    ✅ Higher Than 75 Marks [db2] — approach1

---
+++ solutions/db2/easy/more-than-75-marks/solution-approach1.sql
@@ -0,0 +1,22 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/more-than-75-marks/problem?isFullScreen=true
+-- Problem     Higher Than 75 Marks
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    db2
+-- Status      Accepted
+-- Submitted   2026-09-20, 09:38 p.m.
+-- ──────────────────────────────────────────────────
+
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+SELECT Name
+FROM STUDENTS
+WHERE Marks > 75
+ORDER BY RIGHT(Name, 3), ID;
